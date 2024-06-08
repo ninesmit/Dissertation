13d7005ceae314d9876cfa60e649001b27d5155f
Sarat Smitinont <78297649+ninesmit@users.noreply.github.com>
2024-06-08 15:08:05 +0100

    Update ResNet18_1st_and_2nd_block_removed.py

--- ResNet_Scat/ResNet18_1st_and_2nd_block_removed.py
+++ ResNet_Scat/ResNet18_1st_and_2nd_block_removed.py
@@ -1,3 +1,5 @@
+import time
+import datetime
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -47,21 +49,18 @@ class Scattering2dResNet(nn.Module):
         batch_size, channels, depth, height, width = x.size()
         x = x.view(batch_size, channels * depth, height, width)
         x = self.resnet(x)
-        #print("This is x after resnet", x.shape)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
 
 def train(model, device, train_loader, optimizer, epoch, scattering):
+    train_start_time = time.time()
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         scattering_output = scattering(data)
-        #print("Scat Out", scattering_output.shape)
         optimizer.zero_grad()
         output = model(scattering_output)
-        #print("Output shape", output.shape)
-        #print("Target shape", target.shape)
         loss = F.cross_entropy(output, target)
         loss.backward()
         optimizer.step()
@@ -69,8 +68,14 @@ def train(model, device, train_loader, optimizer, epoch, scattering):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+            
+    train_end_time = time.time()
+    train_duration = datetime.timedelta(seconds=train_end_time - train_start_time)
+    
+    print(f"Training time for epoch:{epoch} is {str(train_duration)}")
 
 def test(model, device, test_loader, scattering):
+    test_start_time = time.time()
     model.eval()
     test_loss = 0
     correct = 0
@@ -81,86 +86,82 @@ def test(model, device, test_loader, scattering):
             test_loss += F.cross_entropy(output, target, reduction='sum').item() # sum up batch loss
             pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
-
+    
     test_loss /= len(test_loader.dataset)
+    
+    test_end_time = time.time()
+    test_duration = datetime.timedelta(seconds=test_end_time - test_start_time)
+    
+    print(f"Testing time for epoch:{epoch+1} is {str(test_duration)}")
+    
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
-if __name__ == '__main__':
+mode = 2
 
-    """Train a simple Hybrid Scattering + ResNet model on CIFAR.
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
 
-        Three models are demoed:
-        'linear' - scattering + linear model
-        'mlp' - scattering + MLP
-        'cnn' - scattering + CNN
+if mode == 1:
+    scattering = Scattering2D(J=2, shape=(128, 128), max_order=1)
+    K = 17*3
+elif mode == 2:
+    scattering = Scattering2D(J=2, shape=(128, 128))
+    K = 81*3
+else:
+    scattering = Scattering2D(J=2, shape=(128, 128))
+    K = 81*3
 
-        scattering 1st order can also be set by the mode
-        Scattering features are normalized by batch normalization.
-        The model achieves around 88% testing accuracy after 10 epochs.
+scattering = scattering.to(device)
 
-        scatter 1st order + linear achieves 64% in 90 epochs
-        scatter 2nd order + linear achieves 70.5% in 90 epochs
+model = Scattering2dResNet(K).to(device)
 
-        scatter + cnn achieves 88% in 15 epochs
+# DataLoaders
+num_workers = 4
+pin_memory = True if use_cuda else False
 
-    """
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
 
-    mode = 2
-    classifier = 'cnn'
-    assert(classifier in ['linear', 'mlp', 'cnn'])
+train_loader = torch.utils.data.DataLoader(
+    datasets.CIFAR10(root=scattering_datasets.get_dataset_dir('CIFAR'), train=True, transform=transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+        transforms.RandomCrop(128, padding=4),
+        transforms.RandomRotation(10),
+        transforms.RandomPerspective(distortion_scale=0.2, p=0.2),
+        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
+        transforms.ToTensor(),
+        normalize,
+    ]), download=True),
+    batch_size=128, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
 
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
+test_loader = torch.utils.data.DataLoader(
+    datasets.CIFAR10(root=scattering_datasets.get_dataset_dir('CIFAR'), train=False, transform=transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+        normalize,
+    ])),
+    batch_size=128, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
 
-    if mode == 1:
-        scattering = Scattering2D(J=2, shape=(128, 128), max_order=1)
-        K = 17*3
-    elif mode == 2:
-        scattering = Scattering2D(J=2, shape=(128, 128))
-        K = 81*3
-    else:
-        scattering = Scattering2D(J=2, shape=(128, 128))
-        K = 81*3
-        
-    scattering = scattering.to(device)
+# Optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 
-    model = Scattering2dResNet(K).to(device)
+total_start_time = time.time()
 
-    # DataLoaders
-    num_workers = 4
-    pin_memory = True if use_cuda else False
+num_epoch = 5
 
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+for epoch in range(0, num_epoch):
+    train(model, device, train_loader, optimizer, epoch+1, scattering)
+    test(model, device, test_loader, scattering)
+    # Save the model every 10 epochs
+    if (epoch + 1) % 5 == 0:
+        torch.save(model.state_dict(), f'Resnet_1st_and_2nd_blocks_removed_epoch_{epoch+1}.pth')
+        print(f'Model saved at epoch {epoch+1}')
 
-    train_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10(root=scattering_datasets.get_dataset_dir('CIFAR'), train=True, transform=transforms.Compose([
-            transforms.Resize((128, 128)),
-            transforms.RandomHorizontalFlip(),
-            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
-            transforms.RandomCrop(128, padding=4),
-            transforms.RandomRotation(10),
-            transforms.RandomPerspective(distortion_scale=0.2, p=0.2),
-            transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
-            transforms.ToTensor(),
-            normalize,
-        ]), download=True),
-        batch_size=128, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+total_end_time = time.time()
+total_elapsed_time = datetime.timedelta(seconds=total_end_time - total_start_time)
 
-    test_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10(root=scattering_datasets.get_dataset_dir('CIFAR'), train=False, transform=transforms.Compose([
-            transforms.Resize((128, 128)),
-            transforms.ToTensor(),
-            normalize,
-        ])),
-        batch_size=128, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
-
-    # Optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
-
-    for epoch in range(0, 150):
-        train(model, device, train_loader, optimizer, epoch+1, scattering)
-        test(model, device, test_loader, scattering)
-
+print(f"\nTotal training time for {num_epoch} epochs: {str(total_elapsed_time)}")
