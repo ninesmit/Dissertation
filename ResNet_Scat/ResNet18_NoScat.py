d9d075f45688d947f2023d2860f858e616f5ef6c
Sarat Smitinont <78297649+ninesmit@users.noreply.github.com>
2024-06-13 12:48:44 +0100

    Update ResNet18_NoScat.py

--- ResNet_Scat/ResNet18_NoScat.py
+++ ResNet_Scat/ResNet18_NoScat.py
@@ -134,7 +134,7 @@ for epoch in range(0, num_epoch):
     
     # Save the model every 20 epochs
     if (epoch + 1) % 20 == 0:
-        torch.save(model.state_dict(), f'Resnet18_epoch_{epoch+1}.pth')
+        torch.save(model.state_dict(), f'Resnet18_NoScat_epoch_{epoch+1}.pth')
         print(f'Model saved at epoch {epoch+1}')
 
 total_end_time = time.time()
