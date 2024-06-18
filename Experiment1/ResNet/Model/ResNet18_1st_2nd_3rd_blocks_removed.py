bf573e35ceac394b8aaa75fc5be29184dd1ab435
Sarat Smitinont <78297649+ninesmit@users.noreply.github.com>
2024-06-18 03:50:00 +0100

    Update ResNet18_1st_2nd_3rd_blocks_removed.py

--- Experiment1/ResNet/Model/ResNet18_1st_2nd_3rd_blocks_removed.py
+++ Experiment1/ResNet/Model/ResNet18_1st_2nd_3rd_blocks_removed.py
@@ -166,7 +166,7 @@ for epoch in range(0, num_epoch):
     
     # Save the model every 20 epochs
     if (epoch + 1) % 20 == 0:
-        torch.save(model.state_dict(), f'Resnet_1st_and_2nd_blocks_removed_epoch_{epoch+1}.pth')
+        torch.save(model.state_dict(), f'Resnet_1st_2nd_3rd_blocks_removed_epoch_{epoch+1}.pth')
         print(f'Model saved at epoch {epoch+1}')
 
 total_end_time = time.time()
